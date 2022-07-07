d80e7e4e50f4c5c6102cce9d7487f9c3a856069f
Rafal <rafalciechanski15@gmail.com>
2022-07-07 12:28:32 +0300

    More work on refactoring

--- 2.6-2.10/src/App.js
+++ 2.6-2.10/src/App.js
@@ -1,9 +1,14 @@
 import { useState } from 'react'
+//import PeopleToShowFilter from './components/Filter'
+import Person from './components/Singleperson.js'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '123456789'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [phoneNo, setNewphone] = useState('')
   const [SearchString, setNewString] = useState('')
@@ -43,7 +48,7 @@ const App = () => {
     setNewString(event.target.value)
     }
 
-  const PeopleToShow = showAll ? persons : persons.filter(person => person.name.includes(SearchString))
+  const PeopleToshow = showAll ? persons : persons.filter(person => person.name.includes(SearchString))
 
 
 
@@ -60,7 +65,10 @@ const App = () => {
         <button type="submit">add</button>
       </form>  
       <h2>Numbers</h2>
-      <p>{PeopleToShow.map(person => <li key={person.name}> {person.name} {person.phone}</li>)}</p>
+      <ul>
+        {PeopleToshow.map(person => <Person key={person.name} person={person}/>
+        )}
+      </ul>
     </div>
   )
 }
